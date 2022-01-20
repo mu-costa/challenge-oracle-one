6346630b11e80ab70c13855ef696f3cd8129fb88
Murilo Costa <mcosta12358@gmail.com>
2022-01-20 20:06:22 -0300

    Implementando a logica da criptografaçao

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 const inputArea = document.querySelector('#input-area');
 const criptografarBotao = document.getElementById('crypto-button');
+let textoNormal;
+let textoCriptografado = [];
 
-criptografarBotao.addEventListener('click',function (){
-    let texto;
-    texto = inputArea.innerHTML;
-    console.log(texto);
-})
+    criptografarBotao.addEventListener('click',()=> {
+        textoNormal = inputArea.value.split('');
+
+        for (let index = 0; index < textoNormal.length; index+=1) {
+
+            if (textoNormal[index] == 'a'){
+                textoCriptografado[index] = 'ai';
+            } else {
+                textoCriptografado[index] = textoNormal[index];
+            }
+        }
+        console.log(textoCriptografado);          
+    })   
 
