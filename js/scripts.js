0cf039e8ad6225eb704cdfd3cee22215f7353751
Murilo Costa <mcosta12358@gmail.com>
2022-01-21 14:32:39 -0300

    logica de criptografia implementada

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,28 @@
 const inputArea = document.querySelector('#input-area');
 const criptografarBotao = document.getElementById('crypto-button');
+const outputArea = document.querySelector('#output-area');
 let textoNormal;
 let textoCriptografado = [];
 
     criptografarBotao.addEventListener('click',()=> {
         textoNormal = inputArea.value.split('');
-
+        //algoritmo para converter o array textoNormal para o array criptografado textoCriptografago
         for (let index = 0; index < textoNormal.length; index+=1) {
 
-            if (textoNormal[index] == 'a'){
+            if (textoNormal[index] == 'a' /* && textoNormal[index] != 'ai' */){
                 textoCriptografado[index] = 'ai';
-            } else {
+            } else if (textoNormal[index] == 'e' /* && textoNormal[index] != 'enter' */){
+                textoCriptografado[index] = 'enter';
+            } else if (textoNormal[index] == 'i' /* && textoNormal[index] != 'imes' */){
+                textoCriptografado[index] = 'imes';
+            } else if (textoNormal[index] == 'o' /* && textoNormal[index] != 'ober' */){
+                textoCriptografado[index] = 'ober';
+            } else if (textoNormal[index] == 'u' /* && textoNormal[index] != 'ufat' */){
+                textoCriptografado[index] = 'ufat';
+            } else{
                 textoCriptografado[index] = textoNormal[index];
             }
         }
-        console.log(textoCriptografado);          
+     outputArea.value = textoCriptografado.join('');        
     })   
 
